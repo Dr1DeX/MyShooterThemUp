be38b24fc3f688e787e49fd60ad983c9df61a8a0
ArtemNester <nesterov.fortech@gmail.com>
2025-09-28 14:48:02 +0300

    landed-damage: Добавлен делегат нанесения урона при падении

--- Source/MyShooterThemUp/Public/Player/STUBaseCharacter.h
+++ Source/MyShooterThemUp/Public/Player/STUBaseCharacter.h
@@ -38,6 +38,12 @@ protected:
     UPROPERTY(EditDefaultsOnly, Category = "Animation")
     UAnimMontage* DeathAnimMontage;
 
+    UPROPERTY(EditDefaultsOnly, Category = "Movement")
+    FVector2D LandedDamageVelocity = FVector2D(900.0f, 1200.0f);
+
+    UPROPERTY(EditDefaultsOnly, Category = "Movement")
+    FVector2D LandedDamage = FVector2D(10.0f, 100.0f);
+
 public:
     virtual void Tick(float DeltaTime) override;
 
@@ -60,4 +66,7 @@ private:
     void OnStopRun();
     void OnDeath();
     void OnHealthChanged(float Health);
+
+    UFUNCTION()
+    void OnGroundLanded(const FHitResult& Hit);
 };
