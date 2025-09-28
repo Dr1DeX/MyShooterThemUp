0660527e0afcb4ce5270db2142d76362b3cd8ecd
Arteom Nesterow <102169514+Dr1DeX@users.noreply.github.com>
2025-09-28 14:48:29 +0300

    landed-damage: Добавлен делегат нанесения урона при падении (#9)

--- Source/MyShooterThemUp/Private/Player/STUBaseCharacter.cpp
+++ Source/MyShooterThemUp/Private/Player/STUBaseCharacter.cpp
@@ -41,6 +41,8 @@ void ASTUBaseCharacter::BeginPlay()
     OnHealthChanged(HealthComponent->GetHealth());
     HealthComponent->OnDeath.AddUObject(this, &ASTUBaseCharacter::OnDeath);
     HealthComponent->OnHealthChanged.AddUObject(this, &ASTUBaseCharacter::OnHealthChanged);
+
+    LandedDelegate.AddDynamic(this, &ASTUBaseCharacter::OnGroundLanded);
 }
 
 void ASTUBaseCharacter::Tick(float DeltaTime)
@@ -125,4 +127,19 @@ void ASTUBaseCharacter::OnDeath()
 void ASTUBaseCharacter::OnHealthChanged(float Health)
 {
     HealthTextComponent->SetText(FText::FromString(FString::Printf(TEXT("%.0f"), Health)));
+}
+
+void ASTUBaseCharacter::OnGroundLanded(const FHitResult& Hit)
+{
+    const auto FallVelocityZ = -GetCharacterMovement()->Velocity.Z;
+    UE_LOG(BaseCharacterLog, Display, TEXT("On landed: %f"), FallVelocityZ);
+
+    if (FallVelocityZ < LandedDamageVelocity.X)
+        return;
+
+    const auto FinalDamage = FMath::GetMappedRangeValueClamped(LandedDamageVelocity, LandedDamage, FallVelocityZ);
+
+    UE_LOG(BaseCharacterLog, Display, TEXT("Final damage landed: %f"), FinalDamage);
+
+    TakeDamage(FinalDamage, FDamageEvent{}, nullptr, nullptr);
 }
