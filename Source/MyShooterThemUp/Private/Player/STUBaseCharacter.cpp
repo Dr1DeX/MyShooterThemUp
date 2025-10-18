af66bd748a80cc56c104624bbd9247e985fd71fc
Arteom Nesterow <102169514+Dr1DeX@users.noreply.github.com>
2025-10-18 15:54:33 +0300

    aimoffset: Добавлен Blendspace для прицеливания, добавлена механика F.A.B.R.I.C (#14)

--- Source/MyShooterThemUp/Private/Player/STUBaseCharacter.cpp
+++ Source/MyShooterThemUp/Private/Player/STUBaseCharacter.cpp
@@ -69,7 +69,8 @@ void ASTUBaseCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCo
     PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ASTUBaseCharacter::Jump);
     PlayerInputComponent->BindAction("Run", IE_Pressed, this, &ASTUBaseCharacter::OnStartRun);
     PlayerInputComponent->BindAction("Run", IE_Released, this, &ASTUBaseCharacter::OnStopRun);
-    PlayerInputComponent->BindAction("Fire", IE_Pressed, WeaponComponent, &USTUWeaponComponent::Fire);
+    PlayerInputComponent->BindAction("Fire", IE_Pressed, WeaponComponent, &USTUWeaponComponent::StartFire);
+    PlayerInputComponent->BindAction("Fire", IE_Released, WeaponComponent, &USTUWeaponComponent::StopFire);
 }
 
 bool ASTUBaseCharacter::IsRunn() const
--- Source/MyShooterThemUp/Public/Components/STUWeaponComponent.h
+++ Source/MyShooterThemUp/Public/Components/STUWeaponComponent.h
@@ -16,7 +16,8 @@ class MYSHOOTERTHEMUP_API USTUWeaponComponent : public UActorComponent
 public:
     USTUWeaponComponent();
 
-    void Fire();
+    void StartFire();
+    void StopFire();
 
 protected:
     virtual void BeginPlay() override;
