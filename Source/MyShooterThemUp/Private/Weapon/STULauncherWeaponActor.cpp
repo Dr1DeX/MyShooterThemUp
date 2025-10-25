e863ab701906c627f57447926abad7d6730d3e42
Arteom Nesterow <102169514+Dr1DeX@users.noreply.github.com>
2025-10-25 22:12:51 +0300

    weapon: Добавлена базовая логика для гранатомета, рефакторинг (#17)

--- Source/MyShooterThemUp/Private/Weapon/STULauncherWeaponActor.cpp
+++ Source/MyShooterThemUp/Private/Weapon/STULauncherWeaponActor.cpp
@@ -1,5 +1,36 @@
 // Shoot Them Up Game, All Rights Reserved.
 
-
 #include "Weapon/STULauncherWeaponActor.h"
+#include "Weapon/STUProjectile.h"
 
+void ASTULauncherWeaponActor::StartFire()
+{
+    MakeShot();
+}
+
+void ASTULauncherWeaponActor::MakeShot()
+{
+
+    UWorld* World = GetWorld();
+    if (!World)
+        return;
+
+    FVector CameraStart, CameraEnd;
+    if (!GetTraceData(CameraStart, CameraEnd))
+        return;
+
+    FShotTraceResult Shot;
+    if (!MakeHit(World, CameraStart, CameraEnd, Shot))
+        return;
+
+    const FVector EndPoint = Shot.bHit ? Shot.Hit.ImpactPoint : Shot.MuzzleEnd;
+    const FVector Direction = (EndPoint - GetMuzzleTM().GetLocation()).GetSafeNormal();
+
+    const FTransform SpawnTransform(FRotator::ZeroRotator, GetMuzzleTM().GetLocation());
+    ASTUProjectile* Projectile = World->SpawnActorDeferred<ASTUProjectile>(ProjectileClass, SpawnTransform);
+    if (Projectile)
+    {
+        Projectile->SetShotDirection(Direction);
+        Projectile->FinishSpawning(SpawnTransform);
+    }
+}
---
+++ Source/MyShooterThemUp/Private/Weapon/STUProjectile.cpp
@@ -0,0 +1,24 @@
+// Shoot Them Up Game, All Rights Reserved.
+
+#include "Weapon/STUProjectile.h"
+#include "Components/SphereComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+
+ASTUProjectile::ASTUProjectile()
+{
+    PrimaryActorTick.bCanEverTick = false;
+
+    CollisionComponent = CreateDefaultSubobject<USphereComponent>("SphereComponent");
+    CollisionComponent->InitSphereRadius(5.0f);
+    SetRootComponent(CollisionComponent);
+
+    MovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>("ProjectileMovementComponent");
+}
+
+void ASTUProjectile::BeginPlay()
+{
+    Super::BeginPlay();
+
+    check(MovementComponent);
+    MovementComponent->Velocity = ShotDirection * MovementComponent->InitialSpeed;
+}
