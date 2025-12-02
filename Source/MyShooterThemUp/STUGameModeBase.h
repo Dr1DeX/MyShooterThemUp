a9c004d48630e07fe58aaba8d9d36278071b3d28
Arteom Nesterow <102169514+Dr1DeX@users.noreply.github.com>
2025-12-02 21:48:52 +0300

    game-mode: Добавлена логика распределения на команды (#30)

--- Source/MyShooterThemUp/STUGameModeBase.h
+++ Source/MyShooterThemUp/STUGameModeBase.h
@@ -4,11 +4,12 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
+#include "STUCoreTypes.h"
 #include "STUGameModeBase.generated.h"
 
-/**
- *
- */
+
+class AAIController;
+
 UCLASS()
 class MYSHOOTERTHEMUP_API ASTUGameModeBase : public AGameModeBase
 {
@@ -16,4 +17,32 @@ class MYSHOOTERTHEMUP_API ASTUGameModeBase : public AGameModeBase
 
 public:
     ASTUGameModeBase();
+
+    virtual void StartPlay() override;
+    virtual UClass* GetDefaultPawnClassForController_Implementation(AController* InController) override;
+
+protected:
+    UPROPERTY(EditDefaultsOnly, Category = "Game")
+    TSubclassOf<AAIController> AIControllerClass;
+
+    UPROPERTY(EditDefaultsOnly, Category = "Game")
+    TSubclassOf<APawn> AIPawnClass;
+    
+    UPROPERTY(EditDefaultsOnly, Category = "Game")
+    FGameData GameData;
+
+private:
+    int32 CurrentRound = 1;
+    int32 RoundCountDown = 0;
+    FTimerHandle GameRoundTimerHandle;
+    
+    void SpawnBots();
+    void StartRound();
+    void GameTimerUpdate();
+    void ResetPlayers();
+    void ResetOnePlayer(AController* Controller);
+
+    void CreateTeamsInfo();
+    FLinearColor DetermineColorByTeamID(int32 TeamID) const;
+    void SetPlayerColor(AController* Controller);
 };
