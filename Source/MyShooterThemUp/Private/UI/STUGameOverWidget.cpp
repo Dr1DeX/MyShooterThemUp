b641d1c6657edbe80e984a18c25873005390eeb8
Dr1DeX <utchihasasuke@yandex.ru>
2025-12-10 17:32:40 +0300

    gameover-ui: Добавлена виджет GameOver/общая статистика за матч

---
+++ Source/MyShooterThemUp/Private/UI/STUGameOverWidget.cpp
@@ -0,0 +1,62 @@
+// Shoot Them Up Game, All Rights Reserved.
+
+
+#include "UI/STUGameOverWidget.h"
+
+#include "STUPlayerStatRowWidget.h"
+#include "STUUtils.h"
+#include "MyShooterThemUp/STUGameModeBase.h"
+#include "Player/STUPlayerState.h"
+#include "Components/VerticalBox.h"
+
+bool USTUGameOverWidget::Initialize()
+{
+    if(GetWorld())
+    {
+        const auto GameMode = Cast<ASTUGameModeBase>(GetWorld()->GetAuthGameMode());
+        if(GameMode)
+        {
+            GameMode->OneMatchStateChanged.AddUObject(this, &USTUGameOverWidget::OnMatchStateChanged);
+        }
+    }
+    return Super::Initialize();
+}
+
+void USTUGameOverWidget::OnMatchStateChanged(ESTUMatchState State)
+{
+    if (State == ESTUMatchState::GameOver)
+    {
+        UpdatePlayerStat();
+    }
+}
+
+void USTUGameOverWidget::UpdatePlayerStat()
+{
+    if(!GetWorld() || !PlayerStatBox)
+        return;
+
+    PlayerStatBox->ClearChildren();
+    
+    for (auto It = GetWorld()->GetControllerIterator(); It; ++It)
+    {
+        const auto Controller = It->Get();
+        if(!Controller)
+            continue;
+
+        const auto PlayerState = Cast<ASTUPlayerState>(Controller->PlayerState);
+        if(!PlayerState)
+            continue;
+
+        const auto PlayerStatRowWidget = CreateWidget<USTUPlayerStatRowWidget>(GetWorld(), PlayerStatRowWidgetClass);
+        if(!PlayerStatRowWidgetClass)
+            continue;
+
+        PlayerStatRowWidget->SetPlayerName(FText::FromString(PlayerState->GetPlayerName()));
+        PlayerStatRowWidget->SetKills(STUUtils::TextFromInt(PlayerState->GetKillsNum()));
+        PlayerStatRowWidget->SetDeaths(STUUtils::TextFromInt(PlayerState->GetDeathsNum()));
+        PlayerStatRowWidget->SetTeam(STUUtils::TextFromInt(PlayerState->GetTeamID()));
+        PlayerStatRowWidget->SetPlayerIndicatorVisibility(Controller->IsPlayerController());
+
+        PlayerStatBox->AddChild(PlayerStatRowWidget);
+    }
+}
