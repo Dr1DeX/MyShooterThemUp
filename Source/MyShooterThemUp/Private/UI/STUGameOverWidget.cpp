f53a63ca601bd8d25f3b47802046c578229cd7b2
Dr1DeX <utchihasasuke@yandex.ru>
2025-12-10 18:19:33 +0300

    reset-level: Добавлен функционал перезагрузки уровня

--- Source/MyShooterThemUp/Private/UI/STUGameOverWidget.cpp
+++ Source/MyShooterThemUp/Private/UI/STUGameOverWidget.cpp
@@ -8,9 +8,12 @@
 #include "MyShooterThemUp/STUGameModeBase.h"
 #include "Player/STUPlayerState.h"
 #include "Components/VerticalBox.h"
+#include "Components/Button.h"
+#include "Kismet/GameplayStatics.h"
 
-bool USTUGameOverWidget::Initialize()
+void USTUGameOverWidget::NativeOnInitialized()
 {
+    Super::NativeOnInitialized();
     if(GetWorld())
     {
         const auto GameMode = Cast<ASTUGameModeBase>(GetWorld()->GetAuthGameMode());
@@ -19,7 +22,10 @@ bool USTUGameOverWidget::Initialize()
             GameMode->OneMatchStateChanged.AddUObject(this, &USTUGameOverWidget::OnMatchStateChanged);
         }
     }
-    return Super::Initialize();
+    if(ResetLevelButton)
+    {
+        ResetLevelButton->OnClicked.AddDynamic(this, &USTUGameOverWidget::OnResetLevel);
+    }
 }
 
 void USTUGameOverWidget::OnMatchStateChanged(ESTUMatchState State)
@@ -59,4 +65,10 @@ void USTUGameOverWidget::UpdatePlayerStat()
 
         PlayerStatBox->AddChild(PlayerStatRowWidget);
     }
+}
+
+void USTUGameOverWidget::OnResetLevel()
+{
+    const FString CurrentLevelName = UGameplayStatics::GetCurrentLevelName(this);
+    UGameplayStatics::OpenLevel(this, FName(CurrentLevelName));
 }
--- Source/MyShooterThemUp/Public/UI/STUGameOverWidget.h
+++ Source/MyShooterThemUp/Public/UI/STUGameOverWidget.h
@@ -8,24 +8,30 @@
 #include "STUGameOverWidget.generated.h"
 
 class UVerticalBox;
+class UButton;
 
 UCLASS()
 class MYSHOOTERTHEMUP_API USTUGameOverWidget : public UUserWidget
 {
 	GENERATED_BODY()
 
-public:
-    virtual bool Initialize() override;
-
 protected:
     UPROPERTY(meta = (BindWidget))
     UVerticalBox* PlayerStatBox;
 
+    UPROPERTY(meta = (BindWidget))
+    UButton* ResetLevelButton;
+    
     UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "UI")
     TSubclassOf<UUserWidget> PlayerStatRowWidgetClass;
+
+    virtual void NativeOnInitialized() override;
 
 private:
     void OnMatchStateChanged(ESTUMatchState State);
     void UpdatePlayerStat();
+
+    UFUNCTION()
+    void OnResetLevel();
     
 };
