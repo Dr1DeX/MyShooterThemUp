8d1043e03359e549d022d2565b5a223e941ddc4a
Arteom Nesterow <102169514+Dr1DeX@users.noreply.github.com>
2025-12-11 20:44:29 +0300

    quit-game: Добавлен виджет выхода игры (#40)

--- Source/MyShooterThemUp/Public/Menu/UI/STUMenuWidget.h
+++ Source/MyShooterThemUp/Public/Menu/UI/STUMenuWidget.h
@@ -16,10 +16,16 @@ class MYSHOOTERTHEMUP_API USTUMenuWidget : public UUserWidget
 protected:
     UPROPERTY(meta = (BindWidget))
     UButton* StartGameButton;
+
+    UPROPERTY(meta = (BindWidget))
+    UButton* QuitGameButton;
     
     virtual void NativeOnInitialized() override;
 
 private:
     UFUNCTION()
     void OnStartGame();
+
+    UFUNCTION()
+    void OnQuitGame();
 };
