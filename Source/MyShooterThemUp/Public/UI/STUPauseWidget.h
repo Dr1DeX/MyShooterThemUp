57a8dd0be3ab1877d4fc676e4fb51704cedb3514
Arteom Nesterow <102169514+Dr1DeX@users.noreply.github.com>
2025-12-13 19:39:19 +0300

    anim-widgets: Добавлена базовая анимация виждетов (#45)

--- Source/MyShooterThemUp/Public/UI/STUPauseWidget.h
+++ Source/MyShooterThemUp/Public/UI/STUPauseWidget.h
@@ -3,13 +3,13 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Blueprint/UserWidget.h"
+#include "STUBaseWidget.h"
 #include "STUPauseWidget.generated.h"
 
 class UButton;
 
 UCLASS()
-class MYSHOOTERTHEMUP_API USTUPauseWidget : public UUserWidget
+class MYSHOOTERTHEMUP_API USTUPauseWidget : public USTUBaseWidget
 {
 	GENERATED_BODY()
 
